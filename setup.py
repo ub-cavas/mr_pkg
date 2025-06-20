93dc844b6c60c8cadf37ec610df4a01cc577ae24
Oakley Thomas <oakleyth@buffalo.edu>
2025-06-20 09:45:32 -0400

    Added missing comma

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'world_transform = mr_pkg.world_transformation:main'
+            'world_transform = mr_pkg.world_transformation:main',
             'virtual_perception = mr_pkg.virtual_perception:main'
         ],
     },
