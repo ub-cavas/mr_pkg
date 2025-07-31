d270d45ff63a5c90658dfb123328ff389748f3ca
Anurag Hruday <anurag.hruday@gmail.com>
2025-07-31 11:04:26 -0400

    Add dual EKF config and launch script

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'mr_pkg'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'world_transform = mr_pkg.world_transformation:main',
+            'dual_ekf = dual_ekf.launch.dual_ekf_navsat:generate_launch_description',
             'virtual_perception = mr_pkg.virtual_perception:main'
         ],
     },
